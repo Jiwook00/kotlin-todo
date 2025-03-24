b1a05bce42114d64b930d9c937e17ea137da7d80
Jiwook00 <jwkim775@gmail.com>
2025-03-24 10:27:03 +0900

    Todo 앱 기본 함수 추가

--- Todo.kt
+++ Todo.kt
@@ -1,8 +1,11 @@
 data class Todo(
     val id: Int,
-    val title: String,
-    val description: String = "",
-    val isCompleted: Boolean = false
+    var title: String,
+    var description: String = "",
+    var isCompleted: Boolean = false
 )
 
-
+// 참고
+//- 아토믹 코틀린 35장: 데이터 클래스
+//- 아토믹 코틀린 04장: var와 val
+//- 아토믹 코틀린 31장: 이름 붙은 인자와 디폴트 인자
--- TodoManager.kt
+++ TodoManager.kt
@@ -3,6 +3,7 @@ class TodoManager {
     private var todoId = 1
 
     fun showMenu() {
+        // 그럴듯한 와일 조건은 없을까?
         while(true) {
             println("\n==== Todo 관리 시스템 ====")
             println("1. 할 일 추가")
@@ -13,10 +14,10 @@ class TodoManager {
             print("원하는 작업을 선택하세요: ")
 
             when (readlnOrNull()) {
-                "1" -> println("addTodo()")
-                "2" -> println("showtodoList()")
-                "3" -> println("toggleTodo()")
-                "4" -> println("deleteTodo()")
+                "1" -> println(addTodo())
+                "2" -> println(showtodoList())
+                "3" -> println(toggleTodo())
+                "4" -> println(deleteTodo())
                 "0" -> return
                 else -> println("잘못된 입력입니다.")
             }
@@ -24,6 +25,76 @@ class TodoManager {
         }
     }
 
+    private fun addTodo() {
+        print("제목을 입력하세요: ")
+        val title = readlnOrNull() ?: ""
+
+        print("설명을 입력하세요(선택사항): ")
+        val description = readlnOrNull() ?: ""
+
+        val todo = Todo(todoId, title, description)
+        todoList.add(todo)
+        todoId++
+
+        println("할 일이 추가되었습니다.")
+    }
+
+    // 할 일 목록 보기
+    private fun showtodoList() {
+        if (todoList.isEmpty()) {
+            println("등록된 할 일이 없습니다.")
+            return
+        }
+
+        println("\n==== 할 일 목록 ====")
+        for ((id, title, description, isCompleted) in todoList) {
+            val status = if (isCompleted) "[완료]" else "[진행중]"
+            println("$id: $status $title - $description")
+        }
+
+    }
+
+    // 할 일 완료, 미완료 토글
+    private fun toggleTodo() {
+        print("완료/미완료 상태를 변경할 할 일의 ID를 입력하세요: ")
+        val id = readlnOrNull()?.toIntOrNull()
+
+        if (id == null) {
+            println("유요한 ID를 입력하세요.")
+            return
+        }
+
+        val todo = todoList.find { it.id == id }
+        // 참고: 44 람다
+        // it 부분 참고.
+        if (todo == null) {
+            println("해당 ID의 할 일을 찾을 수 없습니다.")
+            return
+        }
+
+        val newStatus = !todo.isCompleted
+        todo.isCompleted = newStatus
+        println("""할 일 상태가 "${if (newStatus) "완료" else "미완료"}"로 변경되었습니다. """)
+    }
+
+    // 할 일 삭제
+    private fun deleteTodo() {
+        print("삭제할 할 일의 ID를 입력하세요: ")
+        val id = readlnOrNull()?.toIntOrNull()
+        if (id == null) {
+            println("해당 ID의 할 일을 찾을 수 없습니다.")
+            return
+        }
+
+        val todo = todoList.find { it.id == id }
+        if (todo == null) {
+            println("해당 ID의 할 일을 찾을 수 없습니다.")
+            return
+        }
+
+        todoList.remove(todo)
+        println("할 일이 삭제되었습니다.")
+    }
 
 
 }
